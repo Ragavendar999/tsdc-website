375aac0db507d2545dce3f1b7289b6fa92bc6603
Ragavendar Nandakumar <n.ragavendar@gmail.com>
2025-11-26 10:26:41 +0530

    Fix Meta Pixel raw script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from './components/common/Navbar'
 import ScrollProgress from './components/common/ScrollProgress'
 import './globals.css'
 import Footer from './components/common/Footer'
-import Script from "next/script";
 
 export const metadata = {
   title: 'Traijon Skill Development Center (TSDC)',
@@ -32,53 +31,36 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
-
-      {/* ⭐ HEAD SECTION (Meta Pixel added here) */}
       <head>
-          {/* Preload fbq (Fixes URL Scanner) */}
-  <Script id="fbq-preload" strategy="beforeInteractive">
-    {`
-      !function(){
-        window.fbq = function(){
-          (fbq.callMethod)
-            ? fbq.callMethod.apply(fbq, arguments)
-            : fbq.queue.push(arguments)
-        };
-        if(!window._fbq) window._fbq = fbq;
-        fbq.push = fbq;
-        fbq.loaded = false;
-        fbq.version = '2.0';
-        fbq.queue = [];
-      }();
-    `}
-  </Script>
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            
-            fbq('init', '1310792007726517');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {/* 🔥 Meta Pixel MUST be raw script for Meta URL Scanner */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '1310792007726517');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
 
         <noscript>
-          <img
-            height="1"
-            width="1"
+          <img 
+            height="1" 
+            width="1" 
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1310792007726517&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
 
-      {/* ⭐ BODY SECTION */}
       <body className="bg-white text-gray-900 dark:bg-zinc-900 dark:text-gray-100 transition-colors duration-300">
         <ScrollProgress />
         <Navbar />
