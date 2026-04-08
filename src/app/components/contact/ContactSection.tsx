b657c35eba8acf42a6eb441684e7c07f70d77052
Ragavendar Nandakumar <n.ragavendar@gmail.com>
2026-04-08 16:21:04 +0530

    Update contact page Google Map embed

--- src/app/components/contact/ContactSection.tsx
+++ src/app/components/contact/ContactSection.tsx
@@ -35,7 +35,7 @@ export default function ContactSection() {
 
           <div className="relative h-[320px] overflow-hidden rounded-[2rem] border border-[#d9e2f3] bg-white shadow-[0_30px_70px_rgba(15,23,42,0.1)]">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.4043768777274!2d80.20013457403127!3d12.881699316845065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525b9bc3c5abe1%3A0x319e95489c67101a!2sTraijo%20Skill%20Development%20Center!5e0!3m2!1sen!2sin!4v1752409406800!5m2!1sen!2sin"
+              src="https://www.google.com/maps?q=12.8817134,80.2026107&z=17&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0 }}
